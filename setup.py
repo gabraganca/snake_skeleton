76b47761c530cb2a8e287c370314de5ff7f1e050
Gustavo Braganca <ga.braganca@gmail.com>
2014-12-03 19:19:09 -0200

    Builds a minimal set-up configuration
    Taken from http://www.scotttorborg.com/python-packaging/minimal.html

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Installation Set-Up.
+"""
+from setuptools import setup
+
+setup(name='snake_skeleton',
+      version='0.0.1',
+      description='A skeleton of a snake.',
+      url='http://github.com/gabraganca/skeleton_snake',
+      author=r'Gustavo Bragança',
+      author_email='ga.braganca@gmail.com',
+      license='MIT',
+      packages=['snake_skeleton'],
+      zip_safe=False)
